0161d1d44ffe18e8aa94d21baea409bd74ed0a52
diego <diego@DiegoHA>
2024-04-10 11:20:03 -0300

    Ejercicio 01 resuelto - Aguirre Diego

--- TP01_AguirreDiego/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ TP01_AguirreDiego/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -3,8 +3,16 @@ package ar.edu.unju.fi.ejercicio1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo!!");
+		int n1 = 8;
+		int n2 = 5;
+		int n3 = 1;
+		int n4 = 20;
+		int n5 = 9;
+		
+		double promedio = (double) (n1 + n2 + n3 + n4+ n5)/5;
+		
+		System.out.println(n1+", "+n2+", "+n3+", "+n4+", "+n5);
+		System.out.println("El promedio es: "+promedio);
 	}
 
 }
