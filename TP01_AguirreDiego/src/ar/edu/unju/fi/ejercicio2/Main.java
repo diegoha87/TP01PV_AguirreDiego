74b544906a5b410668db88f01ea97780476abc43
diego <diego@DiegoHA>
2024-04-10 13:53:57 -0300

    Ejercicio 2 resuelto - Aguirre Diego

---
+++ TP01_AguirreDiego/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String pais = "Argentina";
+		long tel = 3885962601l;
+		int edad = 36;
+		double altura_edificio = 12.5;
+		double precio_producto = 500.99;
+		double resultado = Math.cos(0.5);
+
+		System.out.println("Nombre de Pais: "+pais);
+		System.out.println("Telefono: "+tel);
+		System.out.println("Edad: "+edad);
+		System.out.println("Altura Edificio: "+altura_edificio);
+		System.out.println("Precio de Producto: "+precio_producto);
+		System.out.println("Resultado: "+resultado);
+	}
+
+}
