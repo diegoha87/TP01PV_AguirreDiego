1bd6526764094abea57dce01655e0435c89101ea
diego <diego@DiegoHA>
2024-04-10 21:31:54 -0300

    Ejercicio 5 resuelto - Aguirre Diego

---
+++ TP01_AguirreDiego/src/ar/edu/unju/fi/ejercicio5/Main.java
@@ -0,0 +1,30 @@
+package ar.edu.unju.fi.ejercicio5;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int numero;
+		
+		try {
+			System.out.println("Ingrese un numero del 1 al 9: ");
+			numero = scanner.nextInt();
+			if (numero <= 9 && numero >= 1) {
+				for (int i = 0;i <= 10; i++) {
+					System.out.println(i+" x "+numero+" : "+i*numero);
+				}
+			}
+			else
+			{
+				System.out.println("Debe ingresar un numero del 1 al 9.");
+			}
+		}
+		catch (Exception e) {
+			System.out.println("Debe Ingresar un valor numerico.");
+		}
+		
+		
+	}
+
+}
