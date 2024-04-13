f65958502d1d7ca7517c4f2087d34cc7bebdd3da
diego <diego@DiegoHA>
2024-04-13 14:06:02 -0300

    Ejercicio 8 resuelto - Aguirre Diego

---
+++ TP01_AguirreDiego/src/ar/edu/unju/fi/ejercicio8/main/Main.java
@@ -0,0 +1,28 @@
+package ar.edu.unju.fi.ejercicio8.main;
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio8.model.CalculadoraEspecial;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		try {
+			System.out.println("Ingrese un numero entero: ");
+			int numero = scanner.nextInt();
+			CalculadoraEspecial calculadora = new CalculadoraEspecial();
+			calculadora.setN(numero);
+			int sumatoria = calculadora.calcularSumatoria();
+			int productoria = calculadora.calcularProductoria();
+			
+			System.out.println("El calculo de la sumatoria es: "+ sumatoria);
+			System.out.println("El calculo de la productoria es: "+ productoria);
+		}
+		catch (Exception e) {
+			System.out.println("Debe ingresar un valor numérico entero.");
+		}
+		
+
+	}
+
+}
---
+++ TP01_AguirreDiego/src/ar/edu/unju/fi/ejercicio8/model/CalculadoraEspecial.java
@@ -0,0 +1,34 @@
+package ar.edu.unju.fi.ejercicio8.model;
+
+public class CalculadoraEspecial {
+	
+	private int n;
+
+	public int getN() {
+		return n;
+	}
+
+	public void setN(int n) {
+		this.n = n;
+	}
+	
+	public CalculadoraEspecial() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int calcularSumatoria() {
+		int acum=0;
+		for (int k = this.n;0 < k; k--) {
+			acum = acum + (int)Math.pow((k*(k+1))/2, 2);
+		}
+		return acum;
+	}
+	
+	public int calcularProductoria() {
+		int acum=1;
+		for (int k = n;0 < k; k--) {
+			acum = acum * (k*(k+4));
+		}
+		return acum;
+	}
+}
